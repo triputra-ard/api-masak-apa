30760b21dd868e2d5007a0ba96211b5919dd59e7
Tri Putra A <trieputra@live.com>
2022-04-13 12:43:46 +0700

    Test #2

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,8 +1,8 @@
 const baseUrl = require('../constant/url');
 const services = require('../helper/service');
 const cheerio = require('cheerio');
-// const jsdom = require('jsdom');
-// const {JSDOM} = jsdom;
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
 
 const fetchRecipes = (req, res, response) => {
     try {
