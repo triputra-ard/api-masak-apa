01f518aebda255cb9c085552f67269b99e23e976
Tri Putra A <trieputra@live.com>
2022-04-13 12:29:31 +0700

    controller updates

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,8 +1,7 @@
 const baseUrl = require('../constant/url');
 const services = require('../helper/service');
 const cheerio = require('cheerio');
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
+const {JSDOM} = require("jsdom");;
 
 const fetchRecipes = (req, res, response) => {
     try {
